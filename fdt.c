96297500508e6ab71c1947b57ca3a7861070bbd8
ph3b3 <phoebeleong8@gmail.com>
2021-12-18 11:38:03 +1100

    add initial argument checks and fix memory leaks

--- fdt.c
+++ fdt.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <limits.h>
 
-#define FDT_VERSION 2.0
+#define FDT_VERSION 2.1
 
 char* contents[INT_MAX / 2];
 
@@ -33,12 +33,22 @@ int main(int charc, char** charv)
         fclose(file);
     }
 
+    // Compare the filenames and make sure that they're not all the same file
+    for (int i = 1; i < charc; i++)
+    {
+        if (charv[i] != charv[1]) break;
+        if (i == charc - 1)
+        {
+            puts("Your files are the same");
+            return 0;
+        }
+    }
+
     // Open the first file and put it's lines into the "contents" array to be compared
     FILE* file = fopen(charv[1], "r");
     for (int i = 0; i != (INT_MAX / 2) + 1 && feof(file) == 0; i++)
     {
         size_t size = INT_MAX;
-        contents[i] = malloc(size + 1);
         getline(&contents[i], &size, file);
     }
     fclose(file);
@@ -58,11 +68,18 @@ int main(int charc, char** charv)
             {
                 fclose(file);
                 puts("Your files are not the same");
+
+                // getline() malloc()s strings if they're NULL when passed into it, so we're just freeing the allocated memory
+                for (int k = 0; k < sizeof(contents) / sizeof(contents[0]) && contents[k] != NULL; k++) free(contents[k]);
+
                 return 0;
             }
         }
+
         fclose(file);
     }
     puts("Your files are the same");
+
+    for (int k = 0; k < sizeof(contents) / sizeof(contents[0]) && contents[k] != NULL; k++) free(contents[k]);
     return 0;
 }
